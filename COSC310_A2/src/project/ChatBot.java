eb37509da76a02c9442c6a5dac03eee4478eea8e
josepena97 <46974807+josepena97@users.noreply.github.com>
2020-03-11 20:43:37 -0700

    Update ChatBot.java

--- COSC310_A2/src/project/ChatBot.java
+++ COSC310_A2/src/project/ChatBot.java
@@ -19,25 +19,94 @@ public class ChatBot {
 	private static String initial = "Welcome to iRecommend interactive chat bot";
 	private static String[] Goodbye= {"Thank you for using iRecommend, we hope your interaction was succesful"};
 	private static String[] closingStatement = {"Would you like to get another recomendation? (type \"q\" to quit"};
+	public static boolean flag1 = true;
+	public static boolean flag2 = true;
 	
 	
 	public static void main(String[] args) {
 		String input = "";
 		System.out.println(initial);
 		
-		while(input != "q") {
-			System.out.println(Greeting[(rand.nextInt() % 3)]);
-			input = scan.nextLine();
-			
-			if(checkString(input) && checkInput(input)) {
-				
-			}
-			
-		}
-		
+		int count = 0;
+        
+        while (count >= 0){
+        	
+        	if(count == 0) {
+	            System.out.println(Greeting[(rand.nextInt(3))]);
+	        	input = scan.nextLine();
+	        	input.toLowerCase();
+	        	if(checkString(input) == true) {
+	        		if(checkResponse(input, count) == true) {
+	        			count++;
+	        		}
+	        		
+	        	}
+	        	
+        	}
+        	else if(count > 0) {
+        		if(flag1 == true) {
+        			System.out.println(positiveResponse[(rand.nextInt(2))]);
+        		}
+        		else {
+        			System.out.println(negativeResponse[(rand.nextInt(2))]);
+        		}
+        		
+    	        input = scan.nextLine();
+    	        if(checkString(input) == true) {
+	        		if(checkResponse(input, count) == true) {
+	        			if(flag2 == false) {
+	        				System.out.println(Goodbye[0]);
+	        				break;
+	        			}
+	        			WebScrapper w = new WebScrapper();
+	        			System.out.println(Goodbye[0]);
+	        			break;
+	        		}
+    	        }       		
+        	}
+        }
 		scan.close();
 	}
 	
+	public static boolean checkResponse(String str, int count) {
+		if(positiveResponse(str) == true) {
+			if(count == 0) {
+				flag1 = true;
+			}
+			else {
+				flag2 = true;
+			}
+			return true;
+		}
+		else if(negativeResponse(str) == true) {
+			if(count == 0) {
+				flag1 = false;
+			}
+			else {
+				flag2 = false;
+			}
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static boolean positiveResponse(String str) {
+		for (int i = 0; i < positiveInput.length; i++) {
+            if (str.indexOf(positiveInput[i]) != -1)
+                return true;
+        }
+		return false;
+	}
+	
+	public static boolean negativeResponse(String str) {
+		for (int i = 0; i < negativeInput.length; i++){
+            if (str.indexOf(negativeInput[i]) != -1)
+                return true;
+        }
+		return false;
+	}
 	//This method will convert str into a complete lowercase String to standarize all user input to lowercase and 
 	//check if there is any non alphabetical character inside the string in order to ask the user to retype its input
 	//Allows user to input a number as long as its 1 digit long(ie > 9 wont be accepted(couldnt finish))
@@ -52,7 +121,6 @@ public class ChatBot {
 		return true;
 	}
 	
-	public static boolean checkInput(String str) {	
-		
-	}
 }
+
+
