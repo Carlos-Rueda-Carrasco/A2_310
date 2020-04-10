066a716168a445ad7abdaf25210b49a6a1368240
Carlos <carlos.rueda.carrasco@gmail.com>
2020-04-10 14:10:58 -0700

    Update GUI.java
    Comments added

--- COSC310_A2/src/project/GUI.java
+++ COSC310_A2/src/project/GUI.java
@@ -12,20 +12,23 @@ public class GUI {
 	static JTextArea chat;
 	
 	public static void main(String[] args) {
-	
+		// created the java fram
 		JFrame frame = new JFrame("IMDb Chat bot");
+		// dimesions of the window
 		frame.setSize(1000, 800);
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel("Enter text here:");
 		panel.add(label);
-		// accepts up to 100 characters
+		// accepts up to 100 characters from user
 		JTextField text = new JTextField(50);
 		panel.add(text);
 		text.requestFocusInWindow();
+		// button to send message into the chat
 		JButton send = new JButton("Send");
 		panel.add(send);
+		// chat object
 		JTextArea chat = new JTextArea();
-		
+		// listen to when the button is pressed
 		send.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(text.getText().length() < 1) {
@@ -48,4 +51,3 @@ public class GUI {
 	
 	
 }
-
